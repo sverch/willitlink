021aec8bffff0c23f9e4050a42b9e111a18b4c4c
tycho garen <garen@tychoish.com>
2013-11-09 23:24:19 -0500

    combining scons passes

--- willitlink_gen.sh
+++ willitlink_gen.sh
@@ -8,24 +8,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "[wil]: cleanup previous artifacts"
 rm -f $DIR/deps.json $DIR/dep_graph.json $DIR/dependency_tree.txt
 
-# Code to extract the build system dependencies that SCons knows about plus the symbol data in our
-# object files.
-
 # TODO: When I'm doing this in more of a sane way (like integrating with the build system) don't
 # hard code what kind of build I'm doing. Building with SSL now to detect more dependencies.
 
 # Build to make sure we have all our .o files for willitlink.py
-scons -Q --silent --ssl $@ all
-
-# Prints out a "tree" of dependencies to the given file
-scons --ssl $@ --tree=all,prune all >| $DIR/dependency_tree.txt
-
-# Code to extract the dependencies between "*.a" files.  We need to do this because we have our own
+# Also includes special code to extract the dependencies between "*.a" files.
+# We need to do this because we have our own
 # custom thing that handles LIBDEPS specially in site_scons/libdeps.py
+
 git checkout site_scons/libdeps.py
 git apply $DIR/print_scons_libdeps.patch
 scons --ssl $@ all | grep -e "^{" >| $DIR/deps.json
 git checkout site_scons/libdeps.py
+
+# Prints out a "tree" of dependencies to the given file
+scons --ssl $@ --tree=all,prune all >| $DIR/dependency_tree.txt
 
 echo "[wil]: completed data generation, moving to ingestion phase."
 python $DIR/new_ingestion.py -t $DIR/dependency_tree.txt $DIR/deps.json $DIR/dep_graph.json
