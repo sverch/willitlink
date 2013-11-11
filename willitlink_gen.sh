e2af2be2cd8fa4987b4f2f2f4ce7132a23f111aa
Shaun Verch <shaun.verch@10gen.com>
2013-11-11 15:20:33 -0500

    Remove flag to build with ssl

--- willitlink_gen.sh
+++ willitlink_gen.sh
@@ -3,13 +3,13 @@
 # This file is glue holding a bunch of scripts together to generate the symbol and file dependencies
 # for MongoDB
 
+# You can pass build flags after the command to add them to the build.  Recommend building with SSL
+# now to detect more dependencies.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "[wil]: cleanup previous artifacts"
 rm -f $DIR/deps.json $DIR/dep_graph.json $DIR/dependency_tree.txt
-
-# TODO: When I'm doing this in more of a sane way (like integrating with the build system) don't
-# hard code what kind of build I'm doing. Building with SSL now to detect more dependencies.
 
 # Build to make sure we have all our .o files for willitlink.py
 # Also includes special code to extract the dependencies between "*.a" files.
@@ -19,11 +19,11 @@ rm -f $DIR/deps.json $DIR/dep_graph.json $DIR/dependency_tree.txt
 git checkout SConstruct
 git checkout site_scons/libdeps.py
 git apply $DIR/print_scons_libdeps.patch
-scons --ssl $@ all | grep -e "^{" >| $DIR/deps.json
+scons $@ all | grep -e "^{" >| $DIR/deps.json
 git checkout site_scons/libdeps.py
 
 # Prints out a "tree" of dependencies to the given file
-scons --ssl $@ --tree=all,prune all >| $DIR/dependency_tree.txt
+scons $@ --tree=all,prune all >| $DIR/dependency_tree.txt
 
 echo "[wil]: completed data generation, moving to ingestion phase."
 python $DIR/new_ingestion.py -t $DIR/dependency_tree.txt $DIR/deps.json $DIR/dep_graph.json
