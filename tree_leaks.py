441bcd60f402bbfc7c897a7b568fe6f6ab746457
Shaun Verch <shaun.verch@10gen.com>
2013-11-12 17:31:28 -0500

    Added script to find leaks but also show what the symbol is needed by

---
+++ tree_leaks.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+import os
+import sys
+import json
+
+import dep_graph
+from helpers.dev_tools import Timer
+
+def get_full_filenames(g, file_name):
+
+    full_file_names = []
+
+    for i in g.files:
+        if i.endswith(file_name):
+            full_file_names.append(i)
+
+    return full_file_names
+
+# Find all symbols defined in this archive and in all archives that this archive lists as
+# dependencies in scons
+def find_symbol_definitions_recursive(g, full_archive_name):
+
+    # First, we have to get all components of this archive
+    archive_components = g.get('archives_to_components', full_archive_name)
+
+    # Iterate this archive looking at all the symbols we define directly
+    symbols_defined = set()
+    for archive_component in archive_components:
+        try:
+            for symbol_defined in g.get('file_to_symbol_definitions', archive_component):
+                symbols_defined.add(symbol_defined)
+        except KeyError:
+            pass
+
+    # Now, we have to get all archive dependencies of this archive
+    archive_dependencies = []
+    try:
+        archive_dependencies = g.get('target_to_dependencies', full_archive_name)
+    except KeyError:
+        pass
+
+    # Now, recursively call this function on each archive we depend on, aggregating the results in
+    # the symbols found set
+    for archive_dependency in archive_dependencies:
+        for symbol_defined in find_symbol_definitions_recursive(g, archive_dependency):
+            symbols_defined.add(symbol_defined)
+
+    # Return all our symbol definitions for this archive and sub archives
+    return list(symbols_defined)
+
+# Find all symbols needed be this archive, and include the file name
+def find_symbol_dependencies_with_file(g, full_archive_name):
+
+    # First, we have to get all components of this archive
+    archive_components = g.get('archives_to_components', full_archive_name)
+
+    # Iterate this archive looking at all the symbols we depend on directly
+    symbols_needed = set()
+    for archive_component in archive_components:
+        try:
+            for symbol_needed in g.get('file_to_symbol_dependencies', archive_component):
+                symbols_needed.add((symbol_needed, archive_component))
+        except KeyError:
+            pass
+
+    # Return all our symbol dependencies for this archive
+    return list(symbols_needed)
+
+def find_symbol_dependencies(g, full_archive_name):
+    return [ symbol_needed for (symbol_needed, file_needing) in find_symbol_dependencies_with_file(g, full_archive_name) ]
+
+def find_direct_leaks(g, archive_name):
+
+    # Get the full names of this file
+    full_archive_names = get_full_filenames(g, archive_name)
+
+    for full_archive_name in full_archive_names:
+        # Get all symbols needed by this archive
+        symbols_needed = find_symbol_dependencies_with_file(g, full_archive_name)
+
+        # Get all symbols provided by this archive and archives listed as dependencies
+        symbols_found = find_symbol_definitions_recursive(g, full_archive_name)
+
+        # Diff these lists to get the "leaks"
+        leaks = set()
+        for (symbol_needed, file_needing) in symbols_needed:
+            if symbol_needed not in symbols_found:
+                leaks.add((symbol_needed, file_needing))
+
+        o = []
+        for (symbol_leak, file_leaking) in leaks:
+            try:
+                if (len(g.get('symbol_to_file_sources', symbol_leak)) > 0):
+                    result = {}
+                    result['symbol'] = symbol_leak
+                    result['file'] = file_leaking
+                    o.append(result)
+            except KeyError:
+                pass
+
+    return o
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: " + sys.argv[0] + " <archive name>")
+        exit(1)
+
+    pkl_file = os.path.join(os.path.dirname(__file__), "dep_graph.pickle")
+
+    if os.path.exists(pkl_file):
+        data_file = pkl_file
+        print('[wil]: using pickle store')
+    else:
+        data_file = os.path.join(os.path.dirname(__file__), "dep_graph.json")
+        print('[wil]: using json store')
+
+    with Timer('loading data file', True):
+        g = dep_graph.MultiGraph().load(data_file)
+
+    with Timer('leak detection query operation', True):
+        print(json.dumps(find_direct_leaks(g, sys.argv[1]), indent=3))
+
+if __name__ == '__main__':
+    main()
