8f50c1bb2b93cb559f71894667a7edef1fc21b9c
Sam Kleinman <samk@10gen.com>
2013-11-12 14:24:58 -0500

    adding return list operation

--- wil.py
+++ wil.py
@@ -8,20 +8,30 @@ import new_ingestion
 import dep_graph
 from helpers.dev_tools import Timer
 
+def get_graph(args):
+    with Timer('loading graph {0}'.format(args.data), args.timers):
+        g = dep_graph.MultiGraph(timers=args.timers).load(args.data)
+
+    return g
+
 def render_data_for_cli(g, name, rel):
     return json.dumps( { rel: { name: g.get_endswith(rel, name)}}, indent=3)
 
 def get_relationship_node(args):
-    with Timer('loading graph {0}'.format(args.data), args.timers):
-        g = dep_graph.MultiGraph(timers=args.timers).load(args.data)
+    g = get_graph(args)
 
     try:
         print(render_data_for_cli(g, args.name, args.relationship))
     except KeyError:
         print('[wil]: there is no {0} named {1}'.format(args.thing, args.name))
 
+def get_list(args):
+    g = get_graph(args)
+
+    print(json.dumps(getattr(g, args.name)))
+
 def main():
-    default_data_file = os.path.join(os.path.dirname(__file__), "dep_graph.pickle")
+    default_data_file = os.path.join(os.path.dirname(__file__), "dep_graph.json")
 
     relationships = { 'symdep':('symbol_to_file_sources', 'symbol'),
                       'symsrc':('symbol_to_file_dependencies', 'symbol'),
@@ -45,6 +55,10 @@ def main():
         sp.add_argument('name')
         sp.add_argument('--data', '-d', default=default_data_file)
 
+    get_list_parser = subparsers.add_parser('list')
+    get_list_parser.add_argument('name', choices=['symbols', 'files'])
+    get_list_parser.add_argument('--data', '-d', default=default_data_file)
+
     args = parser.parse_args()
 
     operations = {
@@ -56,6 +70,7 @@ def main():
         'targetdep': get_relationship_node,
         'arc': get_relationship_node,
         'deptarget': get_relationship_node,
+        'list': get_list
     }
 
     with Timer('complete operation time', args.timers):
