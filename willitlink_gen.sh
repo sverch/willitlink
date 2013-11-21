1da75cb36c7d5a83fd794344bedb29136a662153
Sam Kleinman <samk@10gen.com>
2013-11-21 10:27:21 -0500

    new collection

--- willitlink_gen.sh
+++ willitlink_gen.sh
@@ -7,20 +7,7 @@
 # now to detect more dependencies.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo "[wil]: cleanup previous artifacts"
-rm -f $DIR/data/deps.json $DIR/data/dep_graph.json $DIR/data/dependency_tree.txt
-
-git checkout SConstruct
-git checkout site_scons/libdeps.py
-# We need to do this because we have our own
-# custom thing that handles LIBDEPS specially in site_scons/libdeps.py
-git apply $DIR/assets/print_scons_libdeps.patch
-# Prints out a "tree" of dependencies to the given file
-scons $@ --tree=all,prune all >| $DIR/data/dependency_tree.txt
-cat $DIR/data/dependency_tree.txt | grep -e "^{" >| $DIR/data/deps.json
-git checkout SConstruct
-git checkout site_scons/libdeps.py
+python $DIR/wil.py collect -m `pwd` --tree_name $DIR/data/dependency_tree.txt --data $DIR/data/deps.json --scons $@
 
 echo "[wil]: completed data generation, moving to ingestion phase."
 python $DIR/wil.py ingest -t -m `pwd` --input_tree $DIR/data/dependency_tree.txt --dep_info $DIR/data/deps.json --output_dep_name $DIR/data/dep_graph.json 
