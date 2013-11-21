825a008a4cc8db11adb6b63490a1e0d5e2243fa2
Sam Kleinman <samk@10gen.com>
2013-11-21 10:53:04 -0500

    reorganized cli interface for relationships and trees

--- wil.py
+++ wil.py
@@ -36,11 +36,33 @@ def get_relationship_node(args):
 
     try:
         name = args.name
-        rel = args.relationship
+
+        rel = None
+        for rel_type in get_relationship_types().keys():
+            if getattr(args, rel_type) is not None:
+                rel = rel_type
+                break
+
+        if rel is None:
+            raise Exception('invalid relationship type.')
 
         render({ rel: { name: g.get_endswith(rel, name)}})
     except KeyError:
         print('[wil]: there is no {0} named {1}'.format(args.thing, args.name))
+
+def get_tree(args):
+    g = get_graph(args)
+
+    for tree_type in get_tree_types().keys():
+        if getattr(args, tree_type) is not None:
+            tree = tree_type
+            args.name = getattr(args, tree)
+            break
+
+    if tree is None:
+        raise Exception('invalid tree type.')
+
+    get_tree_types()[tree](args)
 
 def get_list(args):
     g = get_graph(args)
@@ -120,25 +142,36 @@ def get_unneeded_libdeps(args):
     with Timer('find unneeded libdeps', args.timers):
         render(find_extra_archives(g, args.name))
 
+def get_tree_types():
+    return {
+        'leak': get_leak_check,
+        'symbol': get_symbol_family_tree,
+        'file': get_file_family_tree,
+    }
+
+def get_relationship_types():
+    return { 'symbol_dep':('symbol_to_file_sources', 'symbol', 'the file sources for a symbol'),
+             'symbol_src':('symbol_to_file_dependencies', 'symbol', 'the file(s) that a symbol depends on'),
+             'file_def':('file_to_symbol_definitions', 'file', 'the files that define a symbol'),
+             'file_dep':('file_to_symbol_dependencies', 'file', 'the files that a symbol depends on'),
+             'target_dep':('target_to_dependencies', 'target', 'the dependencies for a build target'),
+             'dep_target':('dependency_to_targets', 'dependency', 'the build targets for a dependency'),
+             'archive':('archives_to_components', 'archive', 'the components of an archive'),
+    }
+
 def main():
     default_cwd = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
     default_data_file = os.path.join(default_cwd, 'data', "dep_graph.json")
-    relationships = { 'symbol-dep':('symbol_to_file_sources', 'symbol', 'the file sources for a symbol'),
-                      'symbol-src':('symbol_to_file_dependencies', 'symbol', 'the file(s) that a symbol depends on'),
-                      'file-def':('file_to_symbol_definitions', 'file', 'the files that define a symbol'),
-                      'file-dep':('file_to_symbol_dependencies', 'file', 'the files that a symbol depends on'),
-                      'target-dep':('target_to_dependencies', 'target', 'the dependencies for a build target'),
-                      'dep-target':('dependency_to_targets', 'dependency', 'the build targets for a dependency'),
-                      'archive':('archives_to_components', 'archive', 'the components of an archive'),
-                    }
 
     parser = argparse.ArgumentParser(prog='wil.py', usage='wil.py [command] [arguments]', add_help='[wil]: a dependency analysis toolkit')
     parser.set_defaults(cwd=default_cwd)
     parser.add_argument('--timers', '-t', default=False, action='store_true')
     subparsers = parser.add_subparsers(dest='command', title='commands', metavar='')
 
+
     ingest_parser = subparsers.add_parser('ingest', help='process collected data')
     ingest_parser = ingestion.argparser(default_cwd, ingest_parser)
+
 
     collector_parser = subparsers.add_parser('collect', help='generate and collect data from the build system')
     collector_parser.set_defaults(data_dir=os.path.join(default_cwd, 'data'))
@@ -147,28 +180,33 @@ def main():
     collector_parser.add_argument('--mongo', '-m', default=os.path.join(default_cwd, '..', 'mongo'))
     collector_parser.add_argument('--scons', '-s', nargs='*', default=[])
 
-    relationship_parser = subparsers.add_parser('relationship', help='return direct single relationships')
-    relationship_parser.add_argument('name', help='the name of object.')
-    relationship_parser.add_argument('--data', '-d', default=default_data_file)
-
-    for k,v in relationships.items():
-        relationship_parser.add_argument(k, help=v[2])
-        relationship_parser.set_defaults(relationship=v[0], thing=v[1])
 
     get_list_parser = subparsers.add_parser('list', help='a list of all symbols or files.')
     get_list_parser.add_argument('type', choices=['symbols', 'files'], help='kind of object')
     get_list_parser.add_argument('filter', help='a prefix limiting expression')
     get_list_parser.add_argument('--data', '-d', default=default_data_file)
 
-    for tree_parser in [ 'symbol-family', 'leak-check', 'file-family']:
-        sp = subparsers.add_parser(tree_parser, help='render tree for ' + tree_parser)
-        sp.add_argument('name')
-        sp.add_argument('depth', type=int)
-        sp.add_argument('--data', '-d', default=default_data_file)
+
+    relationship_parser = subparsers.add_parser('relationship', help='return direct single relationships')
+    relationship_parser.add_argument('--data', '-d', default=default_data_file)
+
+    for k,v in get_relationship_types().items():
+        relationship_parser.add_argument('--' + k, action='store_const', const=v[1], help=v[2] )
+
+    relationship_parser.add_argument('name', help='the name of object.')
+
+    tree_parser = subparsers.add_parser('tree', help='return relationships rendered as trees')
+    tree_parser.add_argument('--data', '-d', default=default_data_file)
+
+    for tree in get_tree_types().keys():
+        tree_parser.add_argument('--' + tree, help='render tree of {0} for an object'.format(tree))
+
+    tree_parser.add_argument('depth', type=int)
+
 
     for query_parser in [ 'leaks', 'direct-leaks', 'symbol',
-                              'extra-libdeps', 'd3-file-family',
-                              'd3-file-family-with-leaks']:
+                          'extra-libdeps', 'd3-file-family',
+                          'd3-file-family-with-leaks']:
         sp = subparsers.add_parser(query_parser, help='query for ' + query_parser)
         sp.add_argument('name')
         sp.add_argument('--data', '-d', default=default_data_file)
@@ -185,21 +223,11 @@ def main():
         'collect': data_collector,
         'ingest': ingestion.command,
 
-        'symbol-dep': get_relationship_node,
-        'symbol-src': get_relationship_node,
-        'file-def': get_relationship_node,
-        'file-dep': get_relationship_node,
-        'target-dep': get_relationship_node,
-        'dep-target': get_relationship_node,
-        'archive': get_relationship_node,
-
+        'relationship': get_relationship_node,
+        'tree': get_tree,
         'list': get_list,
 
         'leaks': get_leaks,
-        'leak-check': get_leak_check,
-
-        'symbol-family': get_symbol_family_tree,
-        'file-family': get_file_family_tree,
 
         'd3-file-family': get_file_family_tree_d3,
         'd3-file-family-with-leaks': get_file_family_tree_with_leaks_d3,
