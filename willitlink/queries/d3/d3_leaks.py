80c5eb108748d6b665dd14923d1c2788306416c9
tycho garen <garen@tychoish.com>
2013-11-22 08:03:15 -0500

    d3 cleanup

--- willitlink/queries/d3/d3_leaks.py
+++ willitlink/queries/d3/d3_leaks.py
@@ -23,30 +23,20 @@ def add_leaks_d3(g, d3_graph_object):
     # For each node in our graph, we want to get symbol dependency information
     for archive_or_executable in d3_graph_object['nodes']:
 
-        try:
-            # First, we have to get all object files directly built into this archive or executable
-            archive_components = g.get('archives_to_components', archive_or_executable)
+        # Get any symbols that are leaking from this archive
+        # Iterate all the symbols leaking from this archive
+        for leak_object in find_direct_leaks(g, archive_or_executable):
 
-            # Get any symbols that are leaking from this archive
-            leaks = find_direct_leaks(g, archive_or_executable)
+            # Get the files this symbol is defined
+            for symbol_source in g.get('symbol_to_file_sources', leak_object['symbol']):
 
-            # Iterate all the symbols leaking from this archive
-            for leak_object in leaks:
-                leak_object['symbol']
-                leak_object['file']
+                # Get the archives this file is in
+                for archive_source in g.get('dependency_to_targets', symbol_source):
 
-                # Get the files this symbol is defined
-                for symbol_source in g.get('symbol_to_file_sources', leak_object['symbol']):
-
-                    # Get the archives this file is in
-                    for archive_source in g.get('dependency_to_targets', symbol_source):
-
-                        # Finally, for each archive, add an edge
-                        d3_graph_object['edges'].append({ 'to' : archive_source,
-                                                          'from' : archive_or_executable,
-                                                          'type' : 'symbol',
-                                                          'symbol' : leak_object['symbol'] })
-        except KeyError:
-            pass
+                    # Finally, for each archive, add an edge
+                    d3_graph_object['edges'].append({ 'to' : archive_source,
+                                                      'from' : archive_or_executable,
+                                                      'type' : 'symbol',
+                                                      'symbol' : leak_object['symbol'] })
 
     return dedupe_edges_d3(d3_graph_object)
--- willitlink/queries/d3/d3_relationship.py
+++ willitlink/queries/d3/d3_relationship.py
@@ -29,32 +29,29 @@ def relationship_info_d3(g, file_names):
 
     full_file_names = get_full_filenames(g, file_names)
 
-    d3_graph_object = {}
-    d3_graph_object['nodes'] = set(full_file_names)
-    d3_graph_object['edges'] = []
+    d3_graph_object = { 'nodes': set(full_file_names),
+                        'edges': [] }
 
     # For each node in our graph, we want to get symbol dependency information
     for archive_or_executable in d3_graph_object['nodes']:
 
         # First, we have to get all symbols directly needed by this archive or executable
-         dependencies = get_symbol_info(g, [ archive_or_executable ], search_depth=1, symbol_type='dependency')
+        # Iterate all the symbols needed by this archive
+        for dependency in get_symbol_info(g, [ archive_or_executable ], search_depth=1, symbol_type='dependency'):
 
-         # Iterate all the symbols needed by this archive
-         for dependency in dependencies:
+            # Get the files this symbol is defined
+            for symbol_source in g.get('symbol_to_file_sources', dependency['symbol']):
 
-             # Get the files this symbol is defined
-             for symbol_source in g.get('symbol_to_file_sources', dependency['symbol']):
+                # Get the archives this file is in
+                for archive_source in g.get('dependency_to_targets', symbol_source):
 
-                 # Get the archives this file is in
-                 for archive_source in g.get('dependency_to_targets', symbol_source):
-
-                     # Finally, for each archive, add an edge, but only if it's in our list of
-                     # file names and it's not ourself
-                     if archive_source in full_file_names and archive_source != archive_or_executable:
-                         d3_graph_object['edges'].append({ 'to' : archive_source,
-                                                         'from' : archive_or_executable,
-                                                         'type' : 'symbol',
-                                                         'symbol' : dependency['symbol'] })
+                    # Finally, for each archive, add an edge, but only if it's in our list of
+                    # file names and it's not ourself
+                    if archive_source in full_file_names and archive_source != archive_or_executable:
+                        d3_graph_object['edges'].append({ 'to' : archive_source,
+                                                          'from' : archive_or_executable,
+                                                          'type' : 'symbol',
+                                                          'symbol' : dependency['symbol'] })
 
     d3_graph_object['nodes'] = list(d3_graph_object['nodes'])
 
