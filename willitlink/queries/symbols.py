5d1cfc10c0265100aaaf207147cce718d9377c83
Shaun Verch <shaun.verch@10gen.com>
2013-11-22 10:42:50 -0500

    Fixing various minor bugs and make symbol list work

--- willitlink/queries/symbols.py
+++ willitlink/queries/symbols.py
@@ -1,8 +1,30 @@
 from willitlink.base.graph import MultiGraph
 
+# Return object format:
+#
+# [{
+#     'symbol' : '<name>',
+#     'type' : 'dependency/definition',
+#     'object' : '<name>',
+#     'path' : {
+#         '<object_name>' : '<object_parent>',
+#         '<object_parent>' : '<object_parent_parent>',
+#         ...
+#     }
+# },
+# ...
+# ]
+
 def locate_symbol(g, symbol_name):
+    symbol_info = []
     for containing_file in g.get('symbol_to_file_sources', symbol_name):
         for containing_archive in g.get('dependency_to_targets', containing_file):
-            print "..." + containing_file[-30:] + " [..." + containing_archive[-30:] + "]"
             for next_containing_archive in g.get('dependency_to_targets', containing_archive):
-                print "[..." + next_containing_archive[-30:] + "]"
+                symbol_info.append({ 'symbol' : symbol_name,
+                    'type' : 'dependency',
+                    'object' : containing_file,
+                    'path' : {
+                        containing_file : containing_archive,
+                        containing_archive : next_containing_archive
+                        } })
+    return symbol_info
