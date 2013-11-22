b4d8691cd8589e8495b067acea793400b04c711c
tycho garen <garen@tychoish.com>
2013-11-22 07:58:57 -0500

    less throwing

--- willitlink/queries/d3/d3_relationship.py
+++ willitlink/queries/d3/d3_relationship.py
@@ -36,34 +36,25 @@ def relationship_info_d3(g, file_names):
     # For each node in our graph, we want to get symbol dependency information
     for archive_or_executable in d3_graph_object['nodes']:
 
-        try:
-            # First, we have to get all symbols directly needed by this archive or executable
-            dependencies = get_symbol_info(g, [ archive_or_executable ], search_depth=1, symbol_type='dependency')
+        # First, we have to get all symbols directly needed by this archive or executable
+         dependencies = get_symbol_info(g, [ archive_or_executable ], search_depth=1, symbol_type='dependency')
 
-            # Iterate all the symbols needed by this archive
-            for dependency in dependencies:
+         # Iterate all the symbols needed by this archive
+         for dependency in dependencies:
 
-                try:
-                    # Get the files this symbol is defined
-                    for symbol_source in g.get('symbol_to_file_sources', dependency['symbol']):
+             # Get the files this symbol is defined
+             for symbol_source in g.get('symbol_to_file_sources', dependency['symbol']):
 
-                        try:
-                            # Get the archives this file is in
-                            for archive_source in g.get('dependency_to_targets', symbol_source):
+                 # Get the archives this file is in
+                 for archive_source in g.get('dependency_to_targets', symbol_source):
 
-                                # Finally, for each archive, add an edge, but only if it's in our list of
-                                # file names and it's not ourself
-                                if archive_source in full_file_names and archive_source != archive_or_executable:
-                                    d3_graph_object['edges'].append({ 'to' : archive_source,
-                                                                    'from' : archive_or_executable,
-                                                                    'type' : 'symbol',
-                                                                    'symbol' : dependency['symbol'] })
-                        except KeyError:
-                            pass
-                except KeyError:
-                    pass
-        except KeyError:
-            pass
+                     # Finally, for each archive, add an edge, but only if it's in our list of
+                     # file names and it's not ourself
+                     if archive_source in full_file_names and archive_source != archive_or_executable:
+                         d3_graph_object['edges'].append({ 'to' : archive_source,
+                                                         'from' : archive_or_executable,
+                                                         'type' : 'symbol',
+                                                         'symbol' : dependency['symbol'] })
 
     d3_graph_object['nodes'] = list(d3_graph_object['nodes'])
 
