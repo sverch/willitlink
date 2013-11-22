44c3f312ea776f80355e291eb87142045c585cdf
tycho garen <garen@tychoish.com>
2013-11-22 07:56:38 -0500

    wrapping

--- willitlink/queries/d3/d3_utils.py
+++ willitlink/queries/d3/d3_utils.py
@@ -8,7 +8,12 @@ def dedupe_edges_d3(d3_graph_object):
     for current_edge in current_edges:
         already_found = False
         for edge_found in edges_found:
-            if current_edge['to'] == edge_found['to'] and current_edge['from'] == edge_found['from'] and ('symbol' not in current_edge or 'symbol' not in edge_found or current_edge['symbol'] == edge_found['symbol']):
+            if (current_edge['to'] == edge_found['to'] and
+                current_edge['from'] == edge_found['from'] and
+                ('symbol' not in current_edge or
+                 'symbol' not in edge_found or
+                 current_edge['symbol'] == edge_found['symbol'])):
+
                 already_found = True
 
         if not already_found:
