1124ecd46858e827458112351f739e07c1f9d0cf
tycho garen <garen@tychoish.com>
2013-12-20 14:52:16 -0500

    testing: adding initial unittest infra

---
+++ test/test_wil.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+class TestWilFunctionality(TestCase):
+    def test_easy(self):
+        self.assertEqual(1, 1)
+
+    def test_hard(self):
+        self.assertEqual(1, 2)
