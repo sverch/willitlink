eb0f8c83a2cff60a7b508dcde5af16dd606d40a9
Shaun Verch <shaun.verch@10gen.com>
2014-01-04 00:10:30 -0800

    Properly skip client_build in file-graph

--- willitlink/queries/file_graph_generator.py
+++ willitlink/queries/file_graph_generator.py
@@ -4,10 +4,12 @@ def generate_file_graph(g):
     file_graph_hash = {}
     for object_file in object_files:
         file_graph_hash[object_file] = {}
+        if 'client_build' in object_file:
+            continue
         for symbol_needed in g.get('file_to_symbol_dependencies', object_file):
-            if 'client_build' in symbol_needed:
-                continue
             for file_providing_symbol in g.get('symbol_to_file_sources', symbol_needed):
+                if 'client_build' in file_providing_symbol:
+                    continue
                 if file_providing_symbol not in file_graph_hash[object_file]:
                     file_graph_hash[object_file][file_providing_symbol] = []
                 file_graph_hash[object_file][file_providing_symbol].append(symbol_needed)
